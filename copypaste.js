b2ac76265234eb041ac8d87be13314fdf9f9cd02
ducasse@vdpresearch.de <philippe@NBPD082025.vdpGruppe.local>
2026-02-09 15:08:29 +0100

    finished ch4

--- copypaste.js
+++ copypaste.js
@@ -159,3 +159,55 @@ function addEventListeners(listeners = {}, el) {
 
   return addedListeners;
 }
+
+function removeEventListeners(listeners = {}, el) {
+  Object.entries(listeners).forEach(([eventName, handler]) => {
+    el.removeEventListener(eventName, handler);
+  });
+}
+
+function destroyDOM(vdom) {
+  const { type } = vdom;
+
+  switch (type) {
+    case DOM_TYPES.TEXT: {
+      removeTextNode(vdom);
+      break;
+    }
+    case DOM_TYPES.ELEMENT: {
+      removeElementNode(vdom);
+      break;
+    }
+    case DOM_TYPES.FRAGMENT: {
+      removeFragmentNode(vdom);
+      break;
+    }
+
+    default: {
+      throw new Error(`Can't destroy DOM of type ${type}`);
+    }
+  }
+
+  delete vdom.el;
+}
+
+function removeTextNode(vdom) {
+  const { el } = vdom;
+  el.remove(); // Documrt API method
+}
+
+function removeElementNode(vdom) {
+  const { el, children, listeners } = vdom;
+  el.remove();
+  children.forEach(destroyDOM);
+
+  if (listeners) {
+    removeEventListeners(listeners, el);
+    delete vdom.listeners;
+  }
+}
+
+function removeFragmentNode(vdom) {
+  const { children } = vdom;
+  children.forEach(destroyDOM);
+}
