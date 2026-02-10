b19ca62ad2cf825bb71bd59cf1aad6181ee96628
ducasse@vdpresearch.de <philippe@NBPD082025.vdpGruppe.local>
2026-02-10 16:26:31 +0100

    started chapter 5

--- copypaste.js
+++ copypaste.js
@@ -211,3 +211,46 @@ function removeFragmentNode(vdom) {
   const { children } = vdom;
   children.forEach(destroyDOM);
 }
+
+class Dispatcher {
+  #subs = new Map();
+  #afterHandlers = [];
+
+  subscribe(commandName, handler) {
+    if (!this.#subs.has(commandName)) {
+      this.#subs.set(commandName, []);
+    }
+
+    const handlers = this.#subs.get(commandName);
+    // checks whether handler is registered
+    // if its already registered do nothing
+    if (handlers.includes(handler)) {
+      return () => {};
+    }
+
+    handlers.push(handler);
+
+    return () => {
+      const idx = handlers.indexOf(handler);
+      handlers.splice(idx, 1);
+    };
+  }
+
+  afterEveryCommand(handler) {
+    this.#afterHandlers.push(handler);
+
+    return () => {
+      const idx = this.#afterHandlers.indexOf(handler);
+      this.#afterHandlers.splice(idx, 1);
+    };
+  }
+
+  dispatch(commandName, payload) {
+    if (this.#subs.has(commandName)) {
+      this.#subs.get(commandName).forEach((handler) => handler(payload));
+    } else {
+      console.warn(`No handlers for command : ${commandName}`);
+    }
+    this.#afterHandlers.forEach((handler) => handler());
+  }
+}
--- packages/runtime/src/utils/arrays.js
+++ packages/runtime/src/utils/arrays.js
@@ -1,4 +1,4 @@
-import { hString } from "./h.js";
+import { hString } from "../h.js";
 
 export function withoutNulls(children) {
   return children.filter((child) => child != null); // removes both null and undefined. !== would remove just null
